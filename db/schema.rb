18b1bb092301227121b3aef01e2509a5d90bc715
Eevan <eevan.redon@gmail.com>
2020-01-08 13:52:52 +0100

    adds performance data model, factory, spec, and hstore

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_121043) do
+ActiveRecord::Schema.define(version: 2020_01_08_124510) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "hstore"
   enable_extension "plpgsql"
+
+  create_table "performance_data", force: :cascade do |t|
+    t.bigint "user_id"
+    t.hstore "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -42,4 +50,5 @@ ActiveRecord::Schema.define(version: 2020_01_08_121043) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "performance_data", "users"
 end
